90ccdf3d10bf546a9bff0bbd0b517904676c7707
Diego Barcellos <diegocb81@gmail.com>
2024-07-18 20:50:31 -0300

    CRUD REST API OK

--- src/main/java/dev/danvega/runnerz/run/RunController.java
+++ src/main/java/dev/danvega/runnerz/run/RunController.java
@@ -1,10 +1,12 @@
 package dev.danvega.runnerz.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -21,9 +23,31 @@ public class RunController {
         return runRepository.findAll();
     }
 
-    @GetMapping("/1")
-    Run findById() {
-        return runRepository.findById(1);
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
+        if(run.isEmpty()) {
+            throw new RunNotFoundException();
+        }
+        return run.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run,id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
     }
 
 }
--- src/main/java/dev/danvega/runnerz/run/RunRepository.java
+++ src/main/java/dev/danvega/runnerz/run/RunRepository.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RunRepository {
@@ -17,11 +18,25 @@ public class RunRepository {
         return runs;
     }
 
-    Run findById(Integer id) {
+    Optional<Run> findById(Integer id) {
         return runs.stream()
                 .filter(run -> run.id() == id)
-                .findFirst()
-                .get();
+                .findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if(existingRun.isPresent()) {
+            runs.set(runs.indexOf(existingRun.get()),run);
+        }
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id().equals(id));
     }
 
     @PostConstruct
@@ -36,7 +51,7 @@ public class RunRepository {
         runs.add(new Run(2,
                 "Wednesday Evening Run",
                 LocalDateTime.now(),
-                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
                 6,
                 Location.INDOOR));
     }
